0caec4d866df890e092e05e3dc16bb61a022a0c0
Erik <128590288+ErikNeph@users.noreply.github.com>
2026-01-10 04:19:53 +0500

    Create combine_names function for full name
    Implement combine_names function to concatenate first and last names.

---
+++ 8kyu/Grasshopper - Combine strings.py
@@ -0,0 +1,12 @@
+"""
+Combine strings function
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters
+(first and last name). The function should return the full name.
+
+Example:
+
+With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+"""
+
+def combine_names(name, last_name):
+    return f"{name} {last_name}"
