59a09b9471d4e27500ac7c96016ce1bbb3ab0606
Erik <128590288+ErikNeph@users.noreply.github.com>
2026-01-22 12:04:46 +0500

    Create Regex count lowercase letters.py

---
+++ 8kyu/Regex count lowercase letters.py
@@ -0,0 +1,20 @@
+"""
+Your task is simply to count the total number of lowercase letters in a string.
+
+Examples
+"abc" ===> 3
+
+"abcABC123" ===> 3
+
+"abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+"" ===> 0;
+
+"ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+"abcdefghijklmnopqrstuvwxyz" ===> 26
+"""
+
+def lowercase_count(string: str) -> int:
+    res = len([char for char in string if char.islower()])
+    return res
