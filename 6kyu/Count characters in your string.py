d92fdb6754a70f9805b46efb4e33ca2d376dda9a
Erik <128590288+ErikNeph@users.noreply.github.com>
2026-01-23 11:30:54 +0500

    Create Count characters in your string.py

---
+++ 6kyu/Count characters in your string.py
@@ -0,0 +1,13 @@
+"""
+The main idea is to count all the occurring characters in a string.
+If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+"""
+
+def count(s: str) -> dict:
+    res_dict = dict.fromkeys(s)
+    for char in s:
+        res_dict[char] = s.count(char)
+        
+    return res_dict
