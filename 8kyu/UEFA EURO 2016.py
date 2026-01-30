6c25736b3b521e8d6d0523d33349187272588ee6
Erik <128590288+ErikNeph@users.noreply.github.com>
2026-01-30 21:21:48 +0500

    Create UEFA EURO 2016.py

---
+++ 8kyu/UEFA EURO 2016.py
@@ -0,0 +1,14 @@
+"""
+Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+uefa_euro_2016(['Germany', 'Ukraine'],[2, 0]) # "At match Germany - Ukraine, Germany won!"
+uefa_euro_2016(['Belgium', 'Italy'],[0, 2]) # "At match Belgium - Italy, Italy won!"
+uefa_euro_2016(['Portugal', 'Iceland'],[1, 1]) # "At match Portugal - Iceland, teams played draw."
+"""
+def uefa_euro_2016(teams: list[str, str], scores: list[int, int]) -> str:
+    if scores[0] > scores[1]:
+        return f"At match {teams[0]} - {teams[1]}, {teams[0]} won!"
+    elif scores[0] < scores[1]:
+        return f"At match {teams[0]} - {teams[1]}, {teams[1]} won!"
+    else:
+        return f"At match {teams[0]} - {teams[1]}, teams played draw."
