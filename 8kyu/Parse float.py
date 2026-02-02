dfd4a36dd4e3af003b1858cbe7039a7284eedaaf
Erik <128590288+ErikNeph@users.noreply.github.com>
2026-02-02 20:29:04 +0500

    Create Parse float.py

---
+++ 8kyu/Parse float.py
@@ -0,0 +1,12 @@
+"""
+Write function parse_float which takes a string/list
+and returns a number or 'none' if conversion is not possible.
+"""
+
+def parse_float(string):
+    try:
+        return float(string)
+    except TypeError:
+        return None
+    except ValueError:
+        return None
